40130cf7b610e81c5b0d1302954f98c001f84b22
Tejasvi2 <tej1234@gmail.com>
2019-12-12 10:27:36 +0530

    signin page with validation added

--- config/passport.js
+++ config/passport.js
@@ -47,3 +47,35 @@ passport.use('local.signup',new LocalStrategy({
         })
     });
 }));
+
+passport.use('local.signin',new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password',
+    passReqToCallback:true 
+},(req,email,password,done)=>{
+    req.checkBody('email','Invalid Email').notEmpty().isEmail();
+    req.checkBody('password','Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if(errors){
+        var messages = [];
+        errors.forEach((error)=>{
+            messages.push(error.msg);
+        });
+        return done(null,false,req.flash('error',messages));
+    }
+
+    User.findOne({'email':email},(err,user)=>{
+        if(err){
+            return done(err);
+        }
+        if(!user){
+            return done(null,false,{message:'User not found'});
+        }
+        if(!user.validPassword(password)){
+            return done(null,false,{message:'Wrong Password'});
+             
+        }
+        return done(null,user);
+      
+    });
+}));
--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,17 @@ router.post('/user/signup',passport.authenticate('local.signup',{
   failureFlash:true
 }));
 
+router.get('/user/signin',(req,res)=>{
+  var messages = req.flash('error')
+  res.render('user/signin',{csrfToken:req.csrfToken(),messages:messages,hasErrors:messages.length>0})
+})
+
+router.post('/user/signin',passport.authenticate('local.signin',{
+  successRedirect:'/user/profile',
+  failureRedirect:'/user/signin',
+  failureFlash:true
+}));
+
 router.get('/user/profile',(req,res)=>{
   res.render('user/profile');
 })
